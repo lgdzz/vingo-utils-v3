0d332d6910c48a4c2408a83a8f8d86b0bc643135
lgdz <xiongwq@foxmail.com>
2025-07-18 17:34:46 +0800

    -

--- captcha/captcha.go
+++ captcha/captcha.go
@@ -8,40 +8,62 @@ package captcha
 
 import (
 	"fmt"
-	"github.com/go-redis/redis"
+	"github.com/lgdzz/vingo-utils-v3/redis"
 	"github.com/mojocn/base64Captcha"
+	"log"
 	"time"
 )
 
 type Captcha struct {
-	Store *RedisStore
+	Enable bool
+	Debug  bool
+	Length int
+	Driver string
+	Store  *RedisStore
 }
 
-func NewCaptcha(client *redis.Client) Captcha {
+func NewCaptcha(api *redis.Api, enable bool) Captcha {
 	return Captcha{
+		Enable: enable,
+		Length: 5,
+		Driver: "digit",
 		Store: &RedisStore{
-			Client: client,
+			Client: api.Client,
 			Prefix: "captcha:",
 			Expire: time.Minute * 5,
 		},
 	}
 }
 
-func (s *Captcha) Generate() map[string]any {
-	driver := base64Captcha.NewDriverDigit(80, 240, 5, 0.7, 80)
+func (s *Captcha) Generate() *map[string]any {
+	if !s.Enable {
+		return nil
+	}
+
+	var driver base64Captcha.Driver
+	switch s.Driver {
+	case "digit":
+		driver = base64Captcha.NewDriverDigit(80, 240, s.Length, 0.7, 80)
+	default:
+		panic("unsupported driver")
+	}
+
 	c := base64Captcha.NewCaptcha(driver, s.Store)
 	id, b64s, answer, err := c.Generate()
 	if err != nil {
 		panic("验证码生成失败")
 	}
-	fmt.Println(id)
-	fmt.Println(answer)
-	return map[string]any{
+	if s.Debug {
+		log.Println(fmt.Sprintf("验证码：%v", answer))
+	}
+	return &map[string]any{
 		"id":   id,
 		"b64s": b64s,
 	}
 }
 
-func (s *Captcha) Verify(id string, answer string) bool {
-	return s.Store.Verify(id, answer, true)
+func (s *Captcha) Verify(id string, answer string) {
+	if s.Enable && !s.Store.Verify(id, answer, true) {
+		panic("验证码不正确")
+	}
 }
--- router/middle.go
+++ router/middle.go
@@ -3,17 +3,31 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/lgdzz/vingo-utils-v3/vingo"
+	"net/http"
 	"time"
 )
 
 func BaseMiddle(hook *Hook) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		startTime := time.Now()
-		c.Set("requestStart", startTime)
-		c.Set("requestUUID", vingo.GetUUID())
+		method := c.Request.Method
+		// 允许的方法
+		switch c.Request.Method {
+		case http.MethodGet, http.MethodPost:
 
-		if hook.BaseMiddle != nil {
-			hook.BaseMiddle(c)
+			startTime := time.Now()
+			c.Set("requestStart", startTime)
+			c.Set("requestUUID", vingo.GetUUID())
+
+			if hook.BaseMiddle != nil {
+				hook.BaseMiddle(c)
+			}
+
+		default:
+			// 禁用 TRACE/TRACK 以及其他未显式允许的方法
+			c.AbortWithStatusJSON(http.StatusForbidden, gin.H{
+				"error": "Forbidden method: " + method,
+			})
+			return
 		}
 	}
 }
