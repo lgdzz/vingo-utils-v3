df653efc8150bef92b8abb03cb0667e2123a7178
lgdzz <xiongwq@foxmail.com>
2026-01-04 16:43:31 +0800

    -

---
+++ email/sender.go
@@ -0,0 +1,16 @@
+// *****************************************************************************
+// 作者: lgdz
+// 创建时间: 2026/1/4
+// 描述：
+// *****************************************************************************
+
+package email
+
+type SenderConfig struct {
+	Host     string `json:"host"`     // SMTP 地址
+	Port     int    `json:"port"`     // SMTP 端口
+	Username string `json:"username"` // 发件人账号
+	Password string `json:"password"` // 发件人密码
+	FromAddr string `json:"fromAddr"` // 发件人邮箱
+	FromName string `json:"fromName"` // 发件人名称
+}
