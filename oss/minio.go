8da84b9083f8e24aad42d1c9d17827d9ff8c19b9
lgdzz <xiongwq@foxmail.com>
2026-05-13 17:04:46 +0800

    -

--- oss/minio.go
+++ oss/minio.go
@@ -7,9 +7,12 @@
 package oss
 
 import (
+	"bytes"
 	"context"
 	"encoding/base64"
 	"fmt"
+	"mime/multipart"
+	"net/http"
 	"strings"
 	"time"
 
@@ -98,17 +101,75 @@ func (s MinIOAdapter) Delete(objectName string) error {
 }
 
 func (s MinIOAdapter) UploadBase64(objectName string, contentType string, fileBase64 string) {
-	var fileBase64Array = strings.Split(fileBase64, ",")
+	//var fileBase64Array = strings.Split(fileBase64, ",")
+	//if len(fileBase64Array) > 1 {
+	//	fileBase64 = fileBase64Array[1]
+	//}
+	//data, err := base64.StdEncoding.DecodeString(fileBase64)
+	//if err != nil {
+	//	panic(err.Error())
+	//}
+	//_, err = s.client.PutObject(context.Background(), s.Config.Bucket, objectName, strings.NewReader(string(data)), int64(len(data)), minio.PutObjectOptions{ContentType: contentType})
+	//if err != nil {
+	//	panic(err.Error())
+	//}
+
+	fileBase64Array := strings.Split(fileBase64, ",")
 	if len(fileBase64Array) > 1 {
 		fileBase64 = fileBase64Array[1]
 	}
+
 	data, err := base64.StdEncoding.DecodeString(fileBase64)
 	if err != nil {
 		panic(err.Error())
 	}
-	_, err = s.client.PutObject(context.Background(), s.Config.Bucket, objectName, strings.NewReader(string(data)), int64(len(data)), minio.PutObjectOptions{ContentType: contentType})
+
+	// 创建 POST Policy
+	policy := minio.NewPostPolicy()
+	_ = policy.SetBucket(s.Config.Bucket)
+	_ = policy.SetKey(objectName)
+	_ = policy.SetExpires(time.Now().Add(time.Hour))
+	_ = policy.SetContentType(contentType)
+
+	uploadURL, formData, err := s.client.PresignedPostPolicy(context.Background(), policy)
 	if err != nil {
 		panic(err.Error())
+	}
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+
+	for k, v := range formData {
+		_ = writer.WriteField(k, v)
+	}
+
+	part, err := writer.CreateFormFile("file", objectName)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	_, err = part.Write(data)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	_ = writer.Close()
+
+	req, err := http.NewRequest("POST", uploadURL.String(), body)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusNoContent && resp.StatusCode != http.StatusCreated {
+		panic("upload failed: " + resp.Status)
 	}
 }
 
